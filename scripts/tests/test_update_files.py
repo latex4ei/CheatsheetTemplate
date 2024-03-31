b03a5e4c675c3d7f92e51c8943c6daf3a090368f
Darius Peters <38152878+dariusptrs@users.noreply.github.com>
2024-03-31 15:37:29 +0200

    use multi-line strings in test_update_files.py and add requirements.txt

--- scripts/tests/test_update_files.py
+++ scripts/tests/test_update_files.py
@@ -20,56 +20,56 @@ def test_check_tex_file_exists__file_does_not_exist__should_raise(fake_base_path
         check_tex_file_exists("nonexistent_repo", Path(fake_base_path))
 
 def test_update_readme_content(fake_base_path, fs):
-    initial_readme_content = (
-        "# WrongTitle\n"
-        "Some content in the README file.\n"
-        "[![Actions Status](https://github.com/latex4ei/WrongCheatsheetTemplate/workflows/CI/badge.svg)](https://github.com/latex4ei/WrongCheatsheetTemplate)\n"
-        "## Section\n"
-        "More details here."
-    )
+    initial_readme_content = """
+        # WrongTitle
+        Some content in the README file.
+        [![Actions Status](https://github.com/latex4ei/WrongCheatsheetTemplate/workflows/CI/badge.svg)](https://github.com/latex4ei/WrongCheatsheetTemplate)
+        ## Section
+        More details here.
+    """
     fs.create_file(fake_base_path / "README.md", contents=initial_readme_content)
     repo_name = "correct_title"
     github_repository = 'user/correct_title'
     
     updated_content = update_readme_content(initial_readme_content, repo_name, github_repository)
     
-    expected_content = (
-        "# correct_title\n"
-        "Some content in the README file.\n"
-        "[![Actions Status](https://github.com/user/correct_title/workflows/CI/badge.svg)](https://github.com/user/correct_title)\n"
-        "## Section\n"
-        "More details here."
-    )
+    expected_content = """
+        # correct_title
+        Some content in the README file.
+        [![Actions Status](https://github.com/user/correct_title/workflows/CI/badge.svg)](https://github.com/user/correct_title)
+        ## Section
+        More details here.
+    """
     assert updated_content == expected_content, "README.md content was not updated correctly."
 
 def test_update_cmake_content(fake_base_path, fs):
-    initial_cmake_content = (
-        "cmake_minimum_required(VERSION 3.12)\n"
-        "project(WrongProjectName NONE)\n"
-        "# Add the main LaTeX document\n"
-        "add_latex_document(\n"
-        "CheatsheetTemplate.tex\n"
-        "AnotherDoc.tex\n"
-        "FORCE_PDF\n"
-        "IMAGE_DIRS img\n"
-        "DEPENDS writegitid\n"
-        ")\n"
-    )
+    initial_cmake_content = """
+        cmake_minimum_required(VERSION 3.12)
+        project(WrongProjectName NONE)
+        # Add the main LaTeX document
+        add_latex_document(
+        CheatsheetTemplate.tex
+        AnotherDoc.tex
+        FORCE_PDF
+        IMAGE_DIRS img
+        DEPENDS writegitid
+        )
+    """
     fs.create_file(fake_base_path / "CMakeLists.txt", contents=initial_cmake_content)
     repo_name = "correct_title"
 
     updated_content = update_cmake_content(initial_cmake_content, repo_name)
     
-    expected_content = (
-        "cmake_minimum_required(VERSION 3.12)\n"
-        "project(correct_title NONE)\n"
-        "# Add the main LaTeX document\n"
-        "add_latex_document(\n"
-        "correct_title.tex\n"
-        "AnotherDoc.tex\n"
-        "FORCE_PDF\n"
-        "IMAGE_DIRS img\n"
-        "DEPENDS writegitid\n"
-        ")\n"
-    )
+    expected_content = """
+        cmake_minimum_required(VERSION 3.12)
+        project(correct_title NONE)
+        # Add the main LaTeX document
+        add_latex_document(
+        correct_title.tex
+        AnotherDoc.tex
+        FORCE_PDF
+        IMAGE_DIRS img
+        DEPENDS writegitid
+        )
+    """
     assert updated_content == expected_content, "CMakeLists.txt content was not updated correctly."
